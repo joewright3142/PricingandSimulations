6887ae1a49d3fa65fa261f74aed15932e75d5720
joewright3142 <61761964+joewright3142@users.noreply.github.com>
2022-08-19 21:32:04 +0100

    Update upload2.py

--- upload2.py
+++ upload2.py
@@ -5,11 +5,13 @@ Stock=100
 time=1
 steps=365*time
 stepsize=1/365
-drift=0.05
-vol=0.1
+drift=0.1
+vol=0.2
 currenttime=0
 
-
+#LogSt-Logs0=
 for i in range(0,steps):
-    Stock=Stock*numpy.exp(stepsize*drift)
+    rand=numpy.random.normal(0,1)
+    Stock=Stock*numpy.exp(stepsize*(drift-0.5*vol**2)+vol*np.sqrt(stepsize)*rand)
+    print(Stock)
     
